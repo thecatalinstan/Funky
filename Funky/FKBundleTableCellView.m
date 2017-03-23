e1454a648d5ce7279357c3b620454194e9bfd9b7
Cătălin Stan <catalin.stan@me.com>
2017-03-23 13:32:48 +0100

    Add custom class for apps table view cell

---
+++ Funky/FKBundleTableCellView.m
@@ -0,0 +1,14 @@
+//
+//  FKBundleTableCellView.m
+//  Funky
+//
+//  Created by Cătălin Stan on 22/03/2017.
+//  Copyright © 2017 Cătălin Stan. All rights reserved.
+//
+
+#import "FKBundleTableCellView.h"
+
+@implementation FKBundleTableCellView
+
+
+@end
